1d80a14bcc199630b67551b4177613877341e941
saulfabreg Wii VC Project <saulfabregamboa@outlook.com>
2025-07-09 12:41:25 -0500

    FBNeo-SS source (from RA-GCN-SS)

---
+++ src/burner/libretro/deps/ugui/SourceHanSansCN_Medium_13X17.c
@@ -0,0 +1,26 @@
+// Converted from SourceHanSansCN_Medium.otf
+//  --size 12.0
+//  --dpi 75
+//  --bpp 8
+
+// For copyright, see original font file.
+
+/************************************************
+Add this lines to ugui_fonts.h:
+  #ifdef UGUI_USE_FONT_SourceHanSansCN_Medium_13X17
+  extern UG_FONT FONT_SourceHanSansCN_Medium_13X17[];
+  #endif
+
+To enable this font, add this line to ugui_config.h:
+  #define UGUI_USE_FONT_SourceHanSansCN_Medium_13X17
+************************************************/
+
+#include "ugui.h"
+#ifdef UGUI_USE_FONT_SourceHanSansCN_Medium_13X17
+
+UG_FONT FONT_SourceHanSansCN_Medium_13X17[] = {
+  // BPP, Width, Height, Chars, Offsets, Range_flags,Bytes per char, Widths presence bit
+  0x01,0x00, // 0xFF5E  65374  '～'
+};
+
+#endif
